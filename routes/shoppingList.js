e403258c94ca75ded54dfd1ca3e7db7672c78e58
Oscar Sanchez <oscsnz@gmail.com>
2024-03-03 08:52:10 -0600

    fixed

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createShoppingListForWeek } = require('../controllers/shoppingListController');
+const { createShoppingListForWeek, getShoppingListByStartDate } = require('../controllers/shoppingListController');
 const authMiddleware = require('../middleware/auth');
 
 // Ruta para crear la lista de compras semanal
 router.post('/create', authMiddleware, createShoppingListForWeek);
+router.get("/byStartDate", authMiddleware, getShoppingListByStartDate)
 
 module.exports = router;
